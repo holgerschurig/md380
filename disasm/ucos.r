6b56ab77e69f96a30a8c29522326333fea14eac4
Holger Schurig <holgerschurig@gmail.com>
2017-04-05 14:55:35 +0200

    disasm: add a script to dissemble the µC/OS-III image

---
+++ disasm/ucos.r
@@ -0,0 +1,17 @@
+e asm.arch = arm
+e asm.cpu = cortex
+e asm.section.sub = true
+
+
+e cfg.fortunes = false
+e asm.lines.ret = true
+e asm.cmtcol = 55
+
+. cpu.r
+f RCC_CR @ 0x40023800
+f RCC_PLLCFGR @ 0x40023804
+f RCC_CFGR @ 0x40023808
+f RCC_CIR @ 0x4002380C
+
+s main
+Pv
---
+++ disasm/ucos.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+r2 -a arm -b 16 -i ucos.r ../obj/ucos.elf $*
