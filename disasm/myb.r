08c44d8913455e7f19ef3d6a3f498c9b631ccb1c
Holger Schurig <holgerschurig@gmail.com>
2017-04-07 13:22:48 +0200

    disasm: more work, and sorted by address

--- disasm/myb.r
+++ disasm/myb.r
@@ -1,29 +1,49 @@
-# Setting the cortex CPU
+# @compile: disasm/checksort.py disasm/myb.r
+
+### Setting up the CPU
+e asm.arch = arm
 e asm.cpu = cortex
-# TODO Setup two RAM regions
-# TODO Emulating memory mapped devices
-# TODO ESIL emulation of Thumb2 code
-# TODO Force filter search hits aligned to 4 bytes
-# TODO Configure sections with iS, S, S=, o.
+e asm.bits = 16
+
+## Setup the other sections
 S 0x08000000 0x08000000 0x0000c000 0x0000c000 boot  -r-x
+Sa arm 16 0x08000000
+S 0x10000000 0x10000000 0x00010000 0x00010000 TCRAM mrw
+S 0x20000000 0x20000000 0x000f0000 0x000f0000 ram   mrwx
 S 0xe0000000 0xe0000000 0x01000000 0x01000000 m4    mrw
 S 0x40000000 0x40000000 0x01000000 0x01000000 stm32 mrw
-S 0x20000000 0x20000000 0x00020000 0x00020000 ram   mrw
-S 0x10000000 0x10000000 0x00010000 0x00010000 TCRAM mrw
-#e asm.section.sub = true
 
-# Generate signures with: ./make_ucos_sigs
-# . sig_ucos.r
+### Setting up the ESIL VM
+e esil.stack.addr = 0x20000000
+e esil.stack.size = 0x000f0000
+aeim
 
+### Misc radare settings
 e cfg.fortunes = false
 e asm.lines.ret = true
 e asm.cmtcol = 55
 e scr.utf8 = 1
+e asm.emustr = true
+#e asm.section.sub = true
+e bin.baddr = 0x80000000
+e io.va = true
+#e anal.hasnext = true
+
+
+### Setup analysis
+# Enable "aa*", which needs a symbol named "entry0"
+f entry0 @ 0x080056a4
+s boot
+e search.in = io.sections.exec
+# later you can try: aa* or (slower) aac
+
+# Generate signures with: ./make_ucos_sigs
+# . sig_ucos.r
+
+
+#############################################################################
 
 # Define a function, optionally analyze it recursively and seek to it
-(f_ addr,)
-(fr addr,)
-(f addr name sz,f $1 $2 @ $0)
 (func addr sz name,f sym.$2 $1 @ $0)
 
 # Define a data section and seek to it
@@ -31,36 +51,15 @@ e scr.utf8 = 1
 (ds addr len lbl,Cs $1 @ $0,f $2 @ $0) # define string
 (dv addr name,f $1 @ $0)                # define variable
 
-. cpu.r
-f RCC_CR @ 0x40023800
-f RCC_PLLCFGR @ 0x40023804
-f RCC_CFGR @ 0x40023808
-f RCC_CIR @ 0x4002380C
-f CPACE @ 0xE000ED88
-f AIRCR @ 0xE000ED0C
-
 . bootloader.r
 
-#############################################################################
-#
-#  Named functions
-#
-#############################################################################
-# Keep names functions near the top, so that they are known when the
-# unnamed functions trigger an afr.
 
-.(dv 0x2000005c DeviceState2)
-.(dv 0x20000077 wLength)
-.(dv 0x20000724 usbd_dfu_AltSet)
-.(dv 0x200011c0 DeviceState1)
-.(dv 0x200011fc Manifest_State)
-.(dv 0x20001200 DeviceStatus)
-.(dv 0x20001204 wBlockNum)
-.(dv 0x20001218 MAL_Buffer)
-.(dv 0x2000122c DeviceState3)
 
 #############################################################################
 # see core_cm4.c
+
+.(d4 0x08000000 98)
+
 .(func 0x08000188 30 NVIC_SystemReset)
 
 #############################################################################
@@ -80,13 +79,13 @@ CCa 0x0800020c USB_REQ_TYPE_MASK
 CCa 0x08000212 USB_REQ_TYPE_STANDARD
 CCa 0x08000216 USB_REQ_TYPE_CLASS
 CCa 0x0800021a req.bRequest
-f case.detach @ 0x8000258
-f case.upload @ 0x800023a
 f case.dnload @ 0x8000234
-f case.clearstatus @ 0x8000246
+f case.upload @ 0x800023a
 f case.getstatus @ 0x8000240
-f case.abort @ 0x8000252
+f case.clearstatus @ 0x8000246
 f case.getstate @ 0x800024c
+f case.abort @ 0x8000252
+f case.detach @ 0x8000258
 f case.default @ 0x800025e
 CCa 0x08000262 USBD_FAIL
 CCa 0x08000266 req.bRequest
@@ -98,7 +97,6 @@ CCa 0x0800027e DFU_DESCRIPTOR_TYPE
 # XXX quite interesting function, because this function
 # XXX decodes the DFU protocol and acts accordingly
 .(func 0x080002c2 1330 EP0_TxSent)
-f loc.ep0txsent.dnbusy @ 0x80007e2
 CCa 0x080002cc STATE_dfuDNBUSY
 CCa 0x080002e4 CMD_GETCOMMANDS
 CCa 0x0800030a CMD_SETADDRESSPOINTER
@@ -115,13 +113,14 @@ CCa 0x0800054a Command 0xC4 with wLength 10
 CCa 0x0800059e Command 0xB2 with wLength 33
 CCa 0x080005e2 Command 0xB3 with wLength 25
 CCa 0x0800062e Command 0xD5 with wLength 513
-# Later there is code with command b4
+f loc.ep0txsent.dnbusy @ 0x80007e2
 CCa 0x080007e8 STATE_dfuMANIFEST
+# Later there is code with command b4
 
 .(func 0x080007f4 4 EP0_RxReady)
 CCa 0x080007f4 USBD_OK
 
-.(func 0x80007f8 162 DFU_Req_DETACH)
+.(func 0x080007f8 162 DFU_Req_DETACH)
 CCa 0x08000802 STATE_dfuIDLE
 CCa 0x0800080c STATE_dfuDNLOAD_SYNC
 CCa 0x08000816 STATE_dfuDNLOAD_IDLE
@@ -148,17 +147,25 @@ f loc.upload_5 @ 0x8000b56
 f loc.upload_7 @ 0x8000bb8
 f loc.upload_x32 @ 0x8000c14
 
-.(dv 0x08000e00 Pointer)
+# TODO .(dv 0x0004b510 Pointer)
+.(d4 0x08000dd4 11)
+
 .(func 0x08000e04 482 DFU_Req_GETSTATUS)
 CCa 0x08000e0a STATE_dfuDNLOAD_SYNC
 CCa 0x08000e0e STATE_dfuMANIFEST_SYNC
 CCa 0x08000e46 CMD_ERASE
 
+.(d4 0x08000fe8 1)
+
 .(func 0x08000fec 100 DFU_Req_CLEARSTATUS)
 
 .(func 0x08001050 12 DFU_Req_GETSTATE)
 
+.(d4 0x0800105c 7)
+
 .(func 0x08001078 98 DFU_Req_ABORT)
+
+.(d4 0x080010dc 1)
 
 .(func 0x080010e0 96 DFU_LeaveDFUMode)
 CCa 0x080010e4 Manifest_complete
@@ -170,8 +177,9 @@ CCa 0x080010e4 Manifest_complete
 #############################################################################
 # see usbd_dfu_mal.c
 
+.(d4 0x0800116c 15)
+
 .(func 0x080011a8 42 MAL_Init)
-.(dv 0x20000114 tMALTab)
 CCa 0x080011c6 tMALTab.pMAL_Init
 
 .(func 0x080011d2 42 MAL_DeInit)
@@ -189,11 +197,13 @@ CCa 0x0800134a tMALTab.pMAL_CheckAdd
 #############################################################################
 # see usbd_req.c
 
+.(func 0x0800132e 20 MAL_CheckAdd)
+
+.(d4 0x0800135c 2)
+
 .(func 0x08001364 88 USBD_StdDevReq)
 
 .(func 0x080013bc 80 USBD_StdItfReq)
-
-.(func 0x0800132e 20 MAL_CheckAdd)
 
 .(func 0x0800140c 360 USBD_StdEPReq)
 
@@ -220,14 +230,14 @@ CCa 0x08001716 USB_OTG_CONFIGURED
 .(func 0x080017b4 56 USBD_GetConfig)
 CCa 0x080017c6 USB_OTG_ADDRESSED
 CCa 0x080017ca USB_OTG_CONFIGURED
-.(dv 0x20001210 USBD_default_cfg)
 
 .(func 0x080017ec 54 USBD_GetStatus)
-.(dv 0x20001214 USBD_cfg_status)
 
 .(func 0x08001822 136 USBD_SetFeature)
 CCa 0x0800182e USB_FEATURE_REMOTE_WAKEUP
 CCa 0x08001848 USB_FEATURE_TEST_MODE
+
+.(d4 0x080018ac 5)
 
 .(func 0x080018c0 52 USBD_ClrFeature)
 CCa 0x080018e2 pdev.dev.class_cb.Setup
@@ -281,36 +291,34 @@ CCa 0x08001a54 USB_OTG_EP0_STATUS_OUT
 #############################################################################
 # no source yet :-(
 
+.(d4 0x08001df0 26)
+
 .(func 0x08001e58 32 FLASH_Unlock)
-f FLASH_KEYR @ 0x40023c04
 
 .(func 0x08001e78 18 XXX_08001e78)
 .(func 0x08001e8a 176 XXX_08001e8a)
 .(func 0x08001f3a 82 XXX_08001f3a)
-f FLASH_CR @ 0x40023c10
 CCa 0x08001f52 clear PSIZE
 CCa 0x08001f5e set PSIZE to program x32
 CCa 0x08001f6a activate flash programming
 CCa 0x08001f7e weird method to clear bit 0 and stop programming
 
 .(func 0x08001f8c 22 FLASH_UnlockOpt)
-f FLASH_OPTCR @ 0x40023c14
-f FLASH_OPTKEYR @ 0x40023c08
 CCa 0x08001f90 isolate the OPTLOCK bit
 
 .(func 0x08001fa2 14 FLASH_LockOpt)
 
 # sym.rdp_lock
-f FLASH_OPTCR_RDP @ 0x40023c15
 CCa 0x08001fd0 set OPTSTRT
 
 .(func 0x08001ffa 58 FLASH_GetFlagStatus)
-f FLASH_SR @ 0x40023c0C
 CCa 0x08002000 isolate BSY bit
 CCa 0x08002004 FLASH_FLAG_BSY
 
 #############################################################################
 # see usb_dcd.c
+
+.(d4 0x08002038 10)
 
 .(func 0x08002088 150 DCD_Init)
 
@@ -322,6 +330,7 @@ CCa 0x08002004 FLASH_FLAG_BSY
 .(func 0x08002288 20 DCD_EP_SetAddress)
 .(func 0x0800229c 22 DevConnect)
 .(func 0x080022b2 22 DCD_DevDisconnect)
+.(d4 0x08002330 1)
 .(func 0x08002374 14 USB_OTG_BSP_mDelay)
 .(func 0x08002612 28 USBD_SetCfg)
 .(func 0x0800262e 16 USBD_ClrCfg)
@@ -329,6 +338,7 @@ CCa 0x08002004 FLASH_FLAG_BSY
 .(func 0x08002822 182 USB_OTG_CoreInit)
 .(func 0x080028d8 24 USB_OTG_EnableGlobalInt)
 .(func 0x080028f0 26 USB_OTG_DisableGlobalInt)
+.(d4 0x08002990 2)
 .(func 0x08002998 54 USB_OTG_SetCurrentMode)
 .(func 0x08002a0e 368 USB_OTG_CoreInitDev)
 .(func 0x08002c4c 114 USB_OTG_EPDeactivate)
@@ -342,12 +352,27 @@ CCa 0x08003048 store current PRIMASK in r0
 CCa 0x0800304c disable IRQ via PRIMASK
 .(func 0x08003050 6 CPU_SR_Restore_)
 .(func 0x08003082 86 vec.PEND_SV)
+f vec.PEND_SV @ 0x08003082
+.(d4 0x080030d8 9)
+.(d4 0x080032b0 11)
+.(d4 0x08003908 4)
+.(d4 0x08003a54 4)
+.(d4 0x08003a78 2)
+.(d4 0x08003c18 5)
+.(d4 0x08003d80 3)
 .(func 0x08003de8 210 otg_fs_int)
 .(func 0x080043f0 388 XXX_080043f0)
-.(d4 0x08005038 13)
 
 #############################################################################
 # see stm32f4xx.c
+
+.(d4 0x08004970 16)
+
+.(d4 0x08004a6c 10)
+
+.(d4 0x08004ae4 1)
+
+.(d4 0x08005038 13)
 
 .(func 0x0800506c 96 SystemInit)
 # Enable FPU
@@ -362,72 +387,21 @@ CCa 0x080050a2 unset HSEBYP
 CCa 0x080050ac weird, reset value already 0x00000000
 CCa 0x080050b0 delay loop
 # Set up vector table
-f VTOR @ 0xe000ed08
 CCa 0x080050c2 Vector Table Offset Register
 CCa 0x080050da set HSEON
 CCa 0x080050e6 check HSERDY
 CCa 0x080050fa HSE_STARTUP_TIMEOUT
 CCa 0x08005104 RCC_CR & RCC_CR_HSERDY) != RESET
 CCa 0x08005114 if (HSEStatus == 0x01)
-f RCC_RCC_APB1ENR @ 0x40023840
 CCa 0x0800511c set PWREN
-f RCC_RCC_APB1ENR @ 0x40023840
 CCa 0x08005128 set PWR_CR_VOS
 
 .(func 0x080050cc 202 SetSysClock)
 CCa 0x080055e4 set DN (Default NaN)
 CCa 0x080055e8 store in FPSCR
 
-# Enable "aa*", which needs a symbol named "entry0"
-f entry0 @ 0x080056a4
-.(func 0x080056a4 8 Reset_Handler)
-CCa 0x080056a4 this is SystemInit as thumb address
-CCa 0x080056a8 this is main as thumb address
-.(d4 0x080056ac 5)
+.(d4 0x08005198 76)
 
-#############################################################################
-
-.(func 0x08005540 32 XXX_08005540)
-.(d4 0x08005560 2)
-.(func 0x08005636 4 store_1_in_r0)
-.(func 0x0800563a 14 XXX_0800563a)
-
-.(func 0x080056c0 16 main)
-.(func 0x080055d4 26 init_fpu)
-CCa 0x080055dc access CPACE
-CCa 0x080055de enable CP10 and CP11 coprocessors (FPU)
-
-.(func 0x08005620 26 main2)
-# Some table is at 0x800558c - 0x80055b8
-s 0x08005540
-af @ 0x08005540
-afvr r1 r1_ptr int
-afvr r4 r4_end int
-#e asm.emu=1
-#e asm.emuwrite=1
-#e io.cache=1
-
-.(d4 0x0800558c 11)
-.(d4 0x08005580 2)
-.(ds 0x080055b8 0x1c str.Radio_USB_Mode)
-.(ds 0x080055f0 0x14 str.Radio_Config)
-.(ds 0x08005608 0x18 str.Radio_Interface)
-.(ds 0x08005660 0x14 str.Anytone)
-.(ds 0x08005674 0x10 str.AnyRoad)
-.(ds 0x08005684 0x10 str.00000000010B)
-.(ds 0x08005694 0x10 str.00000000010C)
-.(ds 0x080054f0 40 str.SPI_Flash_Memory1)
-.(ds 0x08005518 40 str.SPI_Flash_Memory2)
-
-
-#############################################################################
-#
-#  Vectors
-#
-#############################################################################
-# The vectors are described in the PM chapter 12.2
-
-.(d4 0x08000000 98)
 f vec.NMI @ 0x080054b8
 f vec.HARD_FAULT @ 0x080054ba
 f vec.MEM_MANAGE @ 0x080054bc
@@ -435,18 +409,125 @@ f vec.BUS_FAULT @ 0x080054be
 f vec.USAGE_FAULT @ 0x080054c0
 f vec.SVCALL @ 0x080054c2
 f vec.DEBUG_MONITOR @ 0x080054c4
-f vec.PEND_SV @ 0x08003082
 f vec.SYSTICK @ 0x080054c6
+
+f vec.OTG_FS_WKUP @ 0x080054c8
+
+f vec.OTG_FS @ 0x080054ca
+.(d4 0x080054d4 1)
+
+f vec.EXTI3 @ 0x080054d8
+f vec.EXTI2 @ 0x080054da
+f vec.EXTI1 @ 0x080054dc
+f vec.EXTI0 @ 0x080054de
+f vec.TIM4_INT @ 0x080054e0
+f vec.TIM3_INT @ 0x080054e2
+f vec.TIM6_DAC @ 0x080054e4
+f vec.TIM7 @ 0x080054e6
+f vec.TIM8_UP_TIM13 @ 0x080054e8
+f vec.RTC_WKUP @ 0x080054ea
+
+f vec.DMA2_Stream3 @ 0x080054ec
+.(ds 0x080054f0 40 str.SPI_Flash_Memory1)
+.(ds 0x08005518 40 str.SPI_Flash_Memory2)
+
+.(func 0x08005540 32 XXX_weird_jumptable)
+s 0x08001af6
+s 0x08005540
+af 0x08005540
+e asm.emustr=0
+afvr r1 r1_ptr int
+afvr r4 r4_end int
+
+.(d4 0x08005560 2)
+
+.(d4 0x08005580 2)
+
+.(d4 0x0800558c 11)
+f loc.weird_jumptable @ 0x800558c
+
+.(ds 0x080055b8 0x1c str.Radio_USB_Mode)
+
+.(func 0x080055d4 26 init_fpu)
+CCa 0x080055dc access CPACE
+CCa 0x080055de enable CP10 and CP11 coprocessors (FPU)
+
+.(ds 0x080055f0 0x14 str.Radio_Config)
+
+.(ds 0x08005608 0x18 str.Radio_Interface)
+.(func 0x08005620 26 main2)
+
+.(func 0x08005636 4 store_1_in_r0)
+
+.(func 0x0800563a 14 XXX_0800563a)
+
+.(ds 0x08005660 0x14 str.Anytone)
+.(ds 0x08005674 0x10 str.AnyRoad)
+.(ds 0x08005684 0x10 str.00000000010B)
+.(ds 0x08005694 0x10 str.00000000010C)
+
+.(func 0x080056a4 8 Reset_Handler)
+CCa 0x080056a4 this is SystemInit as thumb address
+CCa 0x080056a8 this is main as thumb address
+
+.(d4 0x080056ac 5)
+
+.(d4 0x080056ac 5)
+
+.(func 0x080056c0 16 main)
+
+# 5552: r1 = 2c         
+# 5544: r1 = 5574                     # r1 += pc
+# 5546: r1 = 558c                     # r1 += 0x18
+# 5548: r4 = 54
+# 554a: r4 = 55a2                     # r4 += pc
+# 555c: r4 = 55b8                     # r4 += 0x16
+# 554e: b 555a
+# 555a: cmp r1,r4
+# 555c: bne 5550
+# 5550: r0 = 5590                     # r0 = r1 + 4
+# 5552: r2 = 0xffffc56b               # r2 = [r0], this is -14997
+# 5554: r1 = 1af7                     # r1 = r2 + r1
+# 5556: bl 0x08001af6
+
+# 1af6: r1 = 1000, r0 = 5594          # ldr r1, [r0], 4
+# 1afa: cbz (!r1) end
+# 1afc: r2 = 10000000, r0 = 5598      # ldr r2, [r0], 4
+# 1b00: r3 = 0                        # r2 = r3 << 31
+# 1b02: itt mi
+# 1b04: ignored
+# 1b06: ignored
+# 1b08: r2 = 10000000                 # r2 = r3 + r2
+# 1b0a: r3 = 0
+# 1b0c: r2 = 10000004                 # *r2++ = 0
+# 1b10: r1 = 0ffc
+# 1b12: bne 1b0a
+#       r0 = 55a0?
+
+# The following seems to be wrong :-(
+# 5558: r1 = 55a0
+# 555a: cmp r1, r4
+# 555c: bne 5550
+# 5550: r0 = 55a4                     # r0 = r1 + 4
+# 5552: r2 = 0xffffc4cf               # r2 = [r0], this is -15153
+# 5554: r1 = 1a6f                     # r1 = r2 + r1
+# 5556: bl 0x8001a6e?
+
+# 
+CCa 0x08001af6 clear 0x10000000-0x10001000 and 0x20000138-0x20001230
+CCa 0x0800558c points to 0x08001af6, the function that will interpret this table
+CCa 0x08005590 bytes in TCRAM to clear
+CCa 0x08005594 start in TCRAM to clear
+CCa 0x08005598 bytes in RAM to clear
+CCa 0x0800559c start in RAM to clear
+CCa 0x080055a0 end of table
+
+
 f vec.WWDG @ 0x080056f0
 f vec.PVD @ 0x080056f4
 f vec.TAMP_STAMP @ 0x080056f8
-f vec.RTC_WKUP @ 0x080054ea
 f vec.FLASH @ 0x08005700
 f vec.RCC @ 0x08005704
-f vec.EXTI0 @ 0x080054de
-f vec.EXTI1 @ 0x080054dc
-f vec.EXTI2 @ 0x080054da
-f vec.EXTI3 @ 0x080054d8
 f vec.EXTI4 @ 0x08005718
 f vec.DMA1_Stream0 @ 0x0800571c
 f vec.DMA1_Stream1 @ 0x08005720
@@ -466,8 +547,6 @@ f vec.TIM1_UP_TIM10 @ 0x08005754
 f vec.IM1_TRG_COM_TIM11 @ 0x08005758
 f vec.TIM1_CC @ 0x0800575c
 f vec.TIM2 @ 0x08005760
-f vec.TIM3_INT @ 0x080054e2
-f vec.TIM4_INT @ 0x080054e0
 f vec.I2C1_EV @ 0x0800576c
 f vec.I2C1_ER @ 0x08005770
 f vec.I2C2_EV @ 0x08005774
@@ -479,9 +558,7 @@ f vec.USART2 @ 0x08005788
 f vec.USART3 @ 0x0800578c
 f vec.EXTI5_10 @ 0x08005790
 f vec.RTC_ALARM @ 0x08005794
-f vec.OTG_FS_WKUP @ 0x080054c8
 f vec.TIM8_BRK_TIM12 @ 0x0800579c
-f vec.TIM8_UP_TIM13 @ 0x080054e8
 f vec.TIM8_TRG_COM_TIM14 @ 0x080057a4
 f vec.TIM8_CC @ 0x080057a8
 f vec.DMA1_Stream7 @ 0x080057ac
@@ -491,12 +568,9 @@ f vec.TIM5 @ 0x080057b8
 f vec.SPI3 @ 0x080057bc
 f vec.UART4 @ 0x080057c0
 f vec.UART5 @ 0x080057c4
-f vec.TIM6_DAC @ 0x080054e4
-f vec.TIM7 @ 0x080054e6
 f vec.DMA2_Stream0 @ 0x080057d0
 f vec.DMA2_Stream1 @ 0x080057d4
 f vec.DMA2_Stream2 @ 0x080057d8
-f vec.DMA2_Stream3 @ 0x080054ec
 f vec.DMA2_Stream4 @ 0x080057e0
 f vec.ETH @ 0x080057e4
 f vec.ETH_WKUP @ 0x080057e8
@@ -504,7 +578,6 @@ f vec.CAN2_TX @ 0x080057ec
 f vec.CAN2_RX0 @ 0x080057f0
 f vec.CAN2_RX1 @ 0x080057f4
 f vec.CAN2_SCE @ 0x080057f8
-f vec.OTG_FS @ 0x080054ca
 f vec.DMA2_Stream5 @ 0x08005800
 f vec.DMA2_Stream6 @ 0x08005804
 f vec.DMA2_Stream7 @ 0x08005808
@@ -520,40 +593,57 @@ f vec.CRYP @ 0x0800582c
 f vec.HASH_RNG @ 0x08005830
 f vec.FPU @ 0x08005834
 
+
 #############################################################################
 #
-#  Misc data areas
+#  Memory locations
 #
 #############################################################################
 
-.(d4 0x08000dd4 11)
-.(d4 0x08000fe8 1)
-.(d4 0x0800105c 7)
-.(d4 0x080010dc 1)
-.(d4 0x0800116c 15)
-.(d4 0x0800135c 2)
-.(d4 0x080018ac 5)
-.(d4 0x08001df0 26)
-.(d4 0x08002038 10)
-.(d4 0x08002330 1)
-.(d4 0x08002990 2)
-.(d4 0x080030d8 9)
-.(d4 0x080032b0 11)
-.(d4 0x08003908 4)
-.(d4 0x08003a54 4)
-.(d4 0x08003a78 2)
-.(d4 0x08003c18 5)
-.(d4 0x08003d80 3)
-.(d4 0x08004970 16)
-.(d4 0x08004a6c 10)
-.(d4 0x08004ae4 1)
-.(d4 0x08005198 76)
-.(d4 0x080054d4 1)
-.(d4 0x080056ac 5)
+.(dv 0x2000005c DeviceState2)
+.(dv 0x20000077 wLength)
+.(dv 0x20000114 tMALTab)
+.(dv 0x20000724 usbd_dfu_AltSet)
+.(dv 0x200011c0 DeviceState1)
+.(dv 0x200011fc Manifest_State)
+.(dv 0x20001200 DeviceStatus)
+.(dv 0x20001204 wBlockNum)
+.(dv 0x20001210 USBD_default_cfg)
+.(dv 0x20001214 USBD_cfg_status)
+.(dv 0x20001218 MAL_Buffer)
+.(dv 0x2000122c DeviceState3)
 
 
+#############################################################################
+#
+#  STM43f4 registers
+#
+#############################################################################
 
-aa*
+. cpu.r
+f RCC_CR			@ 0x40023800
+f RCC_PLLCFGR		@ 0x40023804
+f RCC_CFGR			@ 0x40023808
+f RCC_CIR			@ 0x4002380C
+f RCC_APB1ENR       @ 0x40023840
+f FLASH_KEYR		@ 0x40023c04
+f FLASH_OPTKEYR		@ 0x40023c08
+f FLASH_SR			@ 0x40023c0c
+f FLASH_CR			@ 0x40023c10
+f FLASH_OPTCR		@ 0x40023c14
+f FLASH_OPTCR_RDP	@ 0x40023c15
 
-#Vp
-#pd 200
+
+#############################################################################
+#
+#  Cortex-M4 registers
+#
+#############################################################################
+
+f VTOR  @ 0xe000ed08
+f AIRCR @ 0xe000ed0c
+f CPACE @ 0xe000ed88
+
+
+Vp
+#pdf
